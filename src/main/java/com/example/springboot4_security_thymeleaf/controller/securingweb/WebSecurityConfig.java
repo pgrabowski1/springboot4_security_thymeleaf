f452882d102bb6ac5511afb54cc2b5d30bf21374
Piotr <pgbercik@gmail.com>
2026-02-26 21:54:25 +0100

    thymeleaf_and_security add styling to login page, show different menu for logged in and not logged in users

--- src/main/java/com/example/springboot4_security_thymeleaf/controller/securingweb/WebSecurityConfig.java
+++ src/main/java/com/example/springboot4_security_thymeleaf/controller/securingweb/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
 
 @Configuration
 @EnableWebSecurity
@@ -29,7 +29,10 @@ class WebSecurityConfig {
                         .loginPage("/login")
                         .permitAll()
                 )
-                .logout(LogoutConfigurer::permitAll);
+                .logout(h -> h
+                        .logoutSuccessUrl("/")
+                        .permitAll()
+                );
 
 
         return http.build();
@@ -45,6 +48,12 @@ class WebSecurityConfig {
         String password = encoder.encode("password");
         UserDetails user = User.withUsername("user").password(password).roles("USER").build();
         return new InMemoryUserDetailsManager(user);
+    }
+
+
+    @Bean
+    public SpringSecurityDialect springSecurityDialect(){
+        return new SpringSecurityDialect();
     }
 
 }
