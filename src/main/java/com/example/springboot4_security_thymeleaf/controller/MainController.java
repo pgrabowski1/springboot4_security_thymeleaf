440801a974f7ea313e0fed2e5de18899ac0ef8ad
Piotr <pgbercik@gmail.com>
2026-02-26 21:04:40 +0100

    thymeleaf_and_security greeting page

--- src/main/java/com/example/springboot4_security_thymeleaf/controller/MainController.java
+++ src/main/java/com/example/springboot4_security_thymeleaf/controller/MainController.java
@@ -2,6 +2,11 @@ package com.example.springboot4_security_thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 
 @Controller
 public class MainController {
@@ -15,6 +20,12 @@ public class MainController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+    @GetMapping("/greeting")
+    public String greeting(Model model) {
+        model.addAttribute("timestamp", LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+
+        return "greeting";
     }
 }
 
