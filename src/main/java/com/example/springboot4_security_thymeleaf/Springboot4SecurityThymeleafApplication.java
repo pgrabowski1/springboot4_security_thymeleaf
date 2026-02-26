6f9b248309af0f0a78ac4c4e60fb662c2fbb1b50
Piotr <pgbercik@gmail.com>
2026-02-26 20:46:40 +0100

    initial commit

---
+++ src/main/java/com/example/springboot4_security_thymeleaf/Springboot4SecurityThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot4_security_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot4SecurityThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot4SecurityThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot4_security_thymeleaf/Springboot4SecurityThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot4_security_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot4SecurityThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
